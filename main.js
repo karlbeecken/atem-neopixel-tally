b5143d53931a3496d71d712ba37b801ce8a0877a
Karl Beecken <karl@beecken.berlin>
2021-01-22 15:36:09 +0100

    ✨ add led

--- main.js
+++ main.js
@@ -1,19 +1,85 @@
 "use strict";
 exports.__esModule = true;
 var atem_connection_1 = require("atem-connection");
+var ws281x = require("rpi-ws281x");
+var tallyNumber = 1;
+var Led = /** @class */ (function () {
+    function Led() {
+        this.config = {
+            leds: 32,
+            dma: 10,
+            brightness: 127,
+            gpio: 18,
+            type: "grb"
+        };
+        // Number of leds in my strip
+        this.config.leds = 169;
+        // Use DMA 10 (default 10)
+        this.config.dma = 10;
+        // Set full brightness, a value from 0 to 255 (default 255)
+        this.config.brightness = 255;
+        // Set the GPIO number to communicate with the Neopixel strip (default 18)
+        this.config.gpio = 18;
+        // The RGB sequence may vary on some strips. Valid values
+        // are "rgb", "rbg", "grb", "gbr", "bgr", "brg".
+        // Default is "rgb".
+        // RGBW strips are not currently supported.
+        this.config.type = "grb";
+        // Configure ws281x
+        ws281x.configure(this.config);
+    }
+    Led.prototype.red = function () {
+        // Create a pixel array matching the number of leds.
+        // This must be an instance of Uint32Array.
+        var pixels = new Uint32Array(this.config.leds);
+        // Create a fill color with red/green/blue.
+        var red = 255, green = 0, blue = 0;
+        var color = (red << 16) | (green << 8) | blue;
+        for (var i = 0; i < this.config.leds; i++)
+            pixels[i] = color;
+        // Render to strip
+        ws281x.render(pixels);
+    };
+    Led.prototype.green = function () {
+        // Create a pixel array matching the number of leds.
+        // This must be an instance of Uint32Array.
+        var pixels = new Uint32Array(this.config.leds);
+        // Create a fill color with red/green/blue.
+        var red = 0, green = 255, blue = 0;
+        var color = (red << 16) | (green << 8) | blue;
+        for (var i = 0; i < this.config.leds; i++)
+            pixels[i] = color;
+        // Render to strip
+        ws281x.render(pixels);
+    };
+    Led.prototype.off = function () {
+        // Create a pixel array matching the number of leds.
+        // This must be an instance of Uint32Array.
+        var pixels = new Uint32Array(this.config.leds);
+        // Create a fill color with red/green/blue.
+        var red = 0, green = 0, blue = 0;
+        var color = (red << 16) | (green << 8) | blue;
+        for (var i = 0; i < this.config.leds; i++)
+            pixels[i] = color;
+        // Render to strip
+        ws281x.render(pixels);
+    };
+    return Led;
+}());
 var Main = /** @class */ (function () {
     function Main(ip) {
         var _this = this;
         this.ip = ip;
         this.switcher = new atem_connection_1.Atem();
-        this.switcher.on('connected', function () {
+        this.led = new Led();
+        this.switcher.on("connected", function () {
             console.log("Connected to " + _this.ip + ".");
             _this.parseNewState();
-            _this.switcher.on('stateChanged', function (state) {
+            _this.switcher.on("stateChanged", function (state) {
                 _this.parseNewState();
             });
         });
-        this.switcher.on('disconnected', function () {
+        this.switcher.on("disconnected", function () {
             console.log("Disconnected from " + _this.ip + ".");
             _this.connect();
         });
@@ -22,10 +88,24 @@ var Main = /** @class */ (function () {
         // checking out current preview and program information
         var new_preview = this.switcher.listVisibleInputs("preview", 0);
         var new_program = this.switcher.listVisibleInputs("program", 0);
+        var skip = true;
         console.log("PREVIEW: " + new_preview);
+        if (new_preview.includes(tallyNumber)) {
+            console.log("WE ARE PREVIEW");
+            this.led.green();
+            skip = false;
+        }
         this.preview = new_preview;
         console.log("PROGRAM: " + new_program);
+        if (new_program.includes(tallyNumber)) {
+            console.log("WE ARE PROGRAM");
+            this.led.red();
+            skip = false;
+        }
         this.program = new_program;
+        if (skip) {
+            this.led.off();
+        }
     };
     Main.prototype.connect = function () {
         console.log("Connecting to " + this.ip + "...");
--- main.ts
+++ main.ts
@@ -1,56 +1,158 @@
-import { Atem as AtemSwitcher } from 'atem-connection';
+import { Atem as AtemSwitcher } from "atem-connection";
+const ws281x = require("rpi-ws281x");
 
+const tallyNumber: number = 1;
+class Led {
+  config: {
+    leds: number;
+    dma: number;
+    brightness: number;
+    gpio: number;
+    type: string;
+  };
+
+  constructor() {
+    this.config = {
+      leds: 32,
+      dma: 10,
+      brightness: 127,
+      gpio: 18,
+      type: "grb",
+    };
+
+    // Number of leds in my strip
+    this.config.leds = 169;
+
+    // Use DMA 10 (default 10)
+    this.config.dma = 10;
+
+    // Set full brightness, a value from 0 to 255 (default 255)
+    this.config.brightness = 255;
+
+    // Set the GPIO number to communicate with the Neopixel strip (default 18)
+    this.config.gpio = 18;
+
+    // The RGB sequence may vary on some strips. Valid values
+    // are "rgb", "rbg", "grb", "gbr", "bgr", "brg".
+    // Default is "rgb".
+    // RGBW strips are not currently supported.
+    this.config.type = "grb";
+
+    // Configure ws281x
+    ws281x.configure(this.config);
+  }
+
+  red() {
+    // Create a pixel array matching the number of leds.
+    // This must be an instance of Uint32Array.
+    var pixels = new Uint32Array(this.config.leds);
+
+    // Create a fill color with red/green/blue.
+    var red = 255,
+      green = 0,
+      blue = 0;
+    var color = (red << 16) | (green << 8) | blue;
+
+    for (var i = 0; i < this.config.leds; i++) pixels[i] = color;
+
+    // Render to strip
+    ws281x.render(pixels);
+  }
+
+  green() {
+    // Create a pixel array matching the number of leds.
+    // This must be an instance of Uint32Array.
+    var pixels = new Uint32Array(this.config.leds);
+
+    // Create a fill color with red/green/blue.
+    var red = 0,
+      green = 255,
+      blue = 0;
+    var color = (red << 16) | (green << 8) | blue;
+
+    for (var i = 0; i < this.config.leds; i++) pixels[i] = color;
+
+    // Render to strip
+    ws281x.render(pixels);
+  }
+
+  off() {
+    // Create a pixel array matching the number of leds.
+    // This must be an instance of Uint32Array.
+    var pixels = new Uint32Array(this.config.leds);
+
+    // Create a fill color with red/green/blue.
+    var red = 0,
+      green = 0,
+      blue = 0;
+    var color = (red << 16) | (green << 8) | blue;
+
+    for (var i = 0; i < this.config.leds; i++) pixels[i] = color;
+
+    // Render to strip
+    ws281x.render(pixels);
+  }
+}
 class Main {
-
   switcher: AtemSwitcher;
   ip: string;
   preview: any;
   program: any;
+  led: Led;
 
   constructor(ip: string) {
-    this.ip = ip
-    this.switcher = new AtemSwitcher()
+    this.ip = ip;
+    this.switcher = new AtemSwitcher();
+    this.led = new Led();
 
-    this.switcher.on('connected', () => {
+    this.switcher.on("connected", () => {
       console.log(`Connected to ${this.ip}.`);
 
-      this.parseNewState()
+      this.parseNewState();
 
-      this.switcher.on('stateChanged', (state: any) => {
-        this.parseNewState()
+      this.switcher.on("stateChanged", (state: any) => {
+        this.parseNewState();
       });
+    });
 
-    })
-
-    this.switcher.on('disconnected', () => {
+    this.switcher.on("disconnected", () => {
       console.log(`Disconnected from ${this.ip}.`);
       this.connect();
-    })
-
+    });
   }
 
   parseNewState() {
-
     // checking out current preview and program information
-    let new_preview = this.switcher.listVisibleInputs("preview", 0)
-    let new_program = this.switcher.listVisibleInputs("program", 0)
+    let new_preview = this.switcher.listVisibleInputs("preview", 0);
+    let new_program = this.switcher.listVisibleInputs("program", 0);
+    let skip = true;
 
-    console.log(`PREVIEW: ${new_preview}`)
-    this.preview = new_preview
+    console.log(`PREVIEW: ${new_preview}`);
+    if (new_preview.includes(tallyNumber)) {
+      console.log("WE ARE PREVIEW");
+      this.led.green();
+      skip = false;
+    }
+    this.preview = new_preview;
 
-    console.log(`PROGRAM: ${new_program}`)
-    this.program = new_program
+    console.log(`PROGRAM: ${new_program}`);
+    if (new_program.includes(tallyNumber)) {
+      console.log("WE ARE PROGRAM");
+      this.led.red();
+      skip = false;
+    }
+    this.program = new_program;
 
-
-
+    if (skip) {
+      this.led.off();
+    }
   }
 
   connect() {
     console.log(`Connecting to ${this.ip}...`);
-    this.switcher.connect(this.ip)
+    this.switcher.connect(this.ip);
   }
-
 }
 
-let atem = new Main("192.168.178.140")
-atem.connect()
+let atem = new Main("192.168.178.140");
+atem.connect();
